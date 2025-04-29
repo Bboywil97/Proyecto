f1bf898935d04995424b9d7da6f3519845640bea
WILTRAXX <wili190383@Gmail.com>
2025-04-29 11:14:44 -0600

    Actualizar estilos de fondo a oscuro en varias páginas

--- Proyecto/Cliente/src/App.jsx
+++ Proyecto/Cliente/src/App.jsx
@@ -1,14 +1,4 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './pages/Login/Login.jsx'
-import Main from './pages/main/main.jsx'
-=======
 import React from 'react';
-import './App.css';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/useAuth';
@@ -27,7 +17,6 @@ import Pueblo from './pages/Pueblo/Pueblo.jsx';
 import Ciudad from './pages/Ciudad/Ciudad.jsx';
 import Comidas from './pages/Comidas/Comidas.jsx';
 import Hoteles from './pages/Hoteles/Hoteles.jsx';
->>>>>>> Stashed changes
 
 function App() {
   const ProtectedRoutes = () => {
@@ -61,47 +50,6 @@ function App() {
   };
 
   return (
-<<<<<<< Updated upstream
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div>
-                <a href="https://vite.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-              </div>
-              <h1>Vite + React</h1>
-              <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-                </button>
-                <p>
-                  Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-              </div>
-              <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-              </p>
-              <Link to="/login">
-                <button>Go to Login</button>
-              </Link>
-              <Link to="/main">
-                <button>Go to Main</button>
-              </Link>
-            </>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  )
-=======
     <AuthProvider>
       <Router>
         <Routes>
@@ -111,7 +59,6 @@ function App() {
       </Router>
     </AuthProvider>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
